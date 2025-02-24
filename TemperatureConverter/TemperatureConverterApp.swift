018becd2713ccb90857409c1348f53da19be4cf6
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2025-02-24 16:29:30 -0300

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Monique Ferrarini on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
